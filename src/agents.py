675e69aebcd71b0b9f6111acbe634e3f290611d6
Maxat Kulmanov <coolmaksat@gmail.com>
2025-05-26 09:19:55 +0300

    agents

--- src/agents.py
+++ src/agents.py
@@ -1,27 +1,62 @@
 from camel.models import ModelFactory
 from camel.types import ModelPlatformType, ModelType
 from camel.agents import ChatAgent
-from tools import gene_articles_tool
+from tools import genes_articles_tool, aberowl_hpo_tool
 
 model = ModelFactory.create(
   model_platform=ModelPlatformType.OPENROUTER,
-  model_type="google/gemini-2.5-pro-preview",
-  model_config_dict={"temperature": 0.3, "max_tokens": 100000},
+  model_type="google/gemini-2.0-flash-001",
+  model_config_dict={"temperature": 0.3, "max_tokens": 1000000},
 )
 
 gl_agent = ChatAgent(
-    system_message="You are a clinical geneticist analyzing research about genetic variants and rare diseases. Based on the scientific articles",
-    tools=[gene_articles_tool],
+    system_message="You are a clinical geneticist analyzing research about "
+    "genetic variants and rare diseases. Based on the articles tool and background knowledge of phenotypes",
+    model=model,
+    message_window_size=10)
+
+genes_agent = ChatAgent(
+    system_message="You are a helpful assistant that retrieves articles related to genes.",
+    tools=[genes_articles_tool,],
+    model=model,
+    message_window_size=10)
+
+
+pheno_agent = ChatAgent(
+    system_message="You are a helpful assistant that retrieves background knowledge related to phenotypes from AberOWL.",
+    tools=[aberowl_hpo_tool,],
     model=model)
 
-def test_gl_agent():
-    test_genes = "BRCA1, TP53"
-    test_phenotypes = "breast cancer"
-    response = gl_agent.step(
-        f"""Rank the following genes {test_genes} and interpret the evidence for {test_phenotypes}.""")
+
+def test_pheno_agent():
+    test_phenotype = "breast carcinoma"
+    response = pheno_agent.step(
+        f"""Retrieve background knowledge about the phenotype {test_phenotype}.""")
     interpretation = response.msgs[0].content
-    print(f"Genes: {test_genes}, Phenotype: {test_phenotypes}")
+    print(f"Phenotype: {test_phenotype}")
     print(f"Answer: {interpretation}")
+
+def test_gl_agent():
+    test_genes = "IL16, LRIG1, HSPA4, CALU, TAAR3P, HRES1, PPFIBP2, CALML6, PTN, CYP2D6, NDC80, TRBV7-6, MYO5C, GALNT12, PREP, UBC, LINC00471, HMBS, CDH20, PCDHA6, PPP1R2B, RTTN, CYP2U1, OR5B3, RCN3, LCP1, ZNF274, IL37, GSTA2, ADGRF4, CYB561, TRBV7-9, TRPM5, CBX8, TEX11, MRPL2"
+    test_phenotypes = "Microcephaly, Delayed speech and language development, Abnormality of toe, Prominent nasal bridge, 2-3 toe syndactyly, Overlapping toe, Abnormality of skin pigmentation, Pectus excavatum"
+    response = genes_agent.step(
+        f"""Retrieve articles related to the following genes: {test_genes}.
+Generate a context for the model for each gene based on the articles or your knowledge.""")
+    articles = response.msgs[0].content
+    print(articles)
+    response = pheno_agent.step(
+        f"""Retrieve background knowledge about the following phenotypes: {test_phenotypes}.
+Generate a context for the model for each phenotype based on AberOWL or your knowledge.""")
+    phenotypes = response.msgs[0].content
+    print(phenotypes)
+    response = gl_agent.step(
+        f"""Genes context:\n {articles} \n
+Phenotypes context:\n{phenotypes} \n
+Rank the following genes {test_genes} that are directly or indirectly associated with phenotypes: {test_phenotypes}. 
+Generate interpretation for each gene.""")
+    interpretation = response.msgs[0].content
+    print(f"Genes: {test_genes}\nPhenotype: {test_phenotypes}")
+    print(f"Answer:\n{interpretation}")
 
 if __name__ == "__main__":
     test_gl_agent()
--- src/tools.py
+++ src/tools.py
@@ -1,19 +1,54 @@
 from camel.toolkits import FunctionTool
 from retrieval.retriever import ArticleRetriever
-def gene_articles(gene: str) -> str:
+import requests
+import re
+CLEANR = re.compile('<.*?>') 
+from urllib.parse import quote
+
+def cleanhtml(raw_html):
+  cleantext = re.sub(CLEANR, '', raw_html)
+  return cleantext
+
+def genes_articles(genes: list) -> str:
     """Retrieve articles related to a specific gene and format the context for the model
     Args:
-        gene (str): The gene symbol to search for.
+        genes (list): List of gene symbols to search for.
     Returns:
         str: Formatted context string containing article titles, authors, and abstracts.
     """
     retriever = ArticleRetriever()
-    articles = retriever.retrieve(gene)
     context = ""
-    for article in articles:
-        context += f"Title: {article['title']}\n"
-        context += f"Authors: {', '.join(article['authors'])}\n"
-        context += f"Abstract: {article['text']}\n\n"
+    for gene in genes:
+        articles = retriever.retrieve(gene)
+        for article in articles:
+            context += f"Title: {article['title']}\n"
+            context += f"Authors: {', '.join(article['authors'])}\n"
+            context += f"Abstract: {article['text']}\n\n"
     return context.strip()
 
-gene_articles_tool = FunctionTool(gene_articles)
+genes_articles_tool = FunctionTool(genes_articles)
+
+
+def aberowl_hpo(phenotype: str) -> str:
+    """Retrieve background knowledge about a specific phenotype using AberOWL
+    Args:
+        phenotype (str): The phenotype to search for.
+    Returns:
+        str: Background knowledge about the phenotype.
+    """
+    phenotype = phenotype.strip().lower()
+    print("Retrieving background knowledge about phenotype:", phenotype)
+    response = requests.get(f"http://aber-owl.net/api/dlquery",
+                            params=f"axioms=true&labels=true&type=equivalent&query=%27{quote(phenotype)}%27&ontology=HP")
+    response.raise_for_status()
+    data = response.json()
+    if 'status' in data and data['status'] == 'ok' and len(data['result']) > 0:
+        result = data['result'][0]
+        result['id'] = result['class'].replace('http://purl.obolibrary.org/obo/HP_', 'HP:')
+        result['SubClassOf'] = [cleanhtml(item) for item in result.get('SubClassOf', [])]
+        return f"""phenotype: {result['label']} ({result['id']})\n
+Definition: {', '.join(result.get('definition', [])) if 'definition' in result else 'No definition available'}\n
+Synonyms: {', '.join(result.get('synonyms', [])) if 'synonyms' in result else 'No synonyms available'}\n
+Subclass of: {', '.join(result.get('SubClassOf', [])) if 'SubClassOf' in result else 'No superclasses available'}\n"""
+    return f"No background knowledge found for phenotype in AberOWL: {phenotype}"
+aberowl_hpo_tool = FunctionTool(aberowl_hpo)
